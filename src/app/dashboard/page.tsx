d0b489c94bcf7dc3fbdfd8b2fb50365e6c86f357
osengprogrammer <147626933+osengprogrammer@users.noreply.github.com>
2024-01-02 12:34:38 -0800

    okok23

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,8 @@
 import {fetchVoter } from "@/action/fetchingAction";
 import Accordion from "@/components/Accordion/Accordion";
 import { DataTable } from "@/components/DataTable/DataTable";
-import PageContentForm from "@/components/Form/PageContent";
-import ParentComponent from "@/components/Form/ParentForm";
+// import PageContentForm from "@/components/Form/PageContent";
+// import ParentComponent from "@/components/Form/ParentForm";
 import { columns } from "../../components/DataTable/Column";
 import { payments } from "@/components/DataTable/Data";
 import fetchDB from "@/action/fetchDbOne";
